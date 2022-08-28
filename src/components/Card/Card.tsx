fb28d19fe8cdec36de23972a8f5b998f5c2fa88d
ArtsiomZhukouski@  git config --global user.name ArtsiomZhukouskigit config --global user.email you@example.comgit config --global user.email A.Zhukovski@gmail.com <A.Zhukovski@gmail.com>
2022-08-28 19:27:47 +0300

    minor ubdate

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,14 +1,15 @@
 import './Card.css'
-import image from './images/ContentImage.jpg'
+
 
 const CardPostSection = () => {
     return (
         <>
             <div className='card-style'>
                 <div className='image-style'>
-                    {/* <img src={image} alt="" /> */}
+                    <img src='./images/ContentImage.jpg' alt="" />
                 </div>
                 <div className='content-style'>
+                    <p className='content-date'>April 20, 2021</p>
                     Astronauts prep for new solar arrays on nearly seven-hour spacewalk
                 </div>
             </div>
--- src/components/Card/dist/Card.js
+++ src/components/Card/dist/Card.js
@@ -4,7 +4,10 @@ require("./Card.css");
 var CardPostSection = function () {
     return (React.createElement(React.Fragment, null,
         React.createElement("div", { className: 'card-style' },
-            React.createElement("div", { className: 'image-style' }),
-            React.createElement("div", { className: 'content-style' }, "Astronauts prep for new solar arrays on nearly seven-hour spacewalk"))));
+            React.createElement("div", { className: 'image-style' },
+                React.createElement("img", { src: './images/ContentImage.jpg', alt: "" })),
+            React.createElement("div", { className: 'content-style' },
+                React.createElement("p", { className: 'content-date' }, "April 20, 2021"),
+                "Astronauts prep for new solar arrays on nearly seven-hour spacewalk"))));
 };
 exports["default"] = CardPostSection;
